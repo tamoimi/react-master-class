39763559ad3b3277329b3a572084d3b7b6d2a8d6
tami <xorud7979@gmail.com>
2023-03-31 13:40:24 +0900

    remove: delete typescript

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,9 @@ const lightTheme = {
   backgroundColor: "whitesmoke"
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
@@ -25,4 +27,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-                      
